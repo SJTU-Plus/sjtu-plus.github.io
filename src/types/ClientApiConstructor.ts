7c23c30d9f911360f64fbc895acad64d5e8b0566
Alex Chi <iskyzh@gmail.com>
2020-08-13 00:36:26 +0800

    use jekyll

--- src/types/ClientApiConstructor.ts
+++
@@ -1,7 +0,0 @@
-import { VueConstructor } from "vue";
-import { ClientContext } from "./ClientContext";
-
-export type ClientApiConstructor = (
-  Vue: VueConstructor,
-  context: ClientContext
-) => void;
--- src/types/ClientContext.ts
+++
@@ -1,8 +0,0 @@
-import VueRouter from "vue-router";
-import { MetaInfo } from "vue-meta";
-
-export interface ClientContext {
-  appOptions: object;
-  router: VueRouter;
-  head: MetaInfo;
-}
